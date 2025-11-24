9c2fa2951eb0b5e9390d4abdf39e5e72eec3c086
Jenothan <esanjenothan@gmail.com>
2025-11-24 11:13:10 +0530

    commit

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <html>
 
 <head>
-<script src="https://cdn.tailwindcss.com"></script>
+	<script src="https://cdn.tailwindcss.com"></script>
 
-	   <style>
+	<style>
 		.hide-scrollbar::-webkit-scrollbar {
-				display: none;
-			}
-		</style>
-		
+			display: none;
+		}
+	</style>
+
 </head>
 
-<body class="bg-light select-none">
+<body class="bg-light select-none hide-scrollbar">
 	<?php
 	include('auth/auth-session.php');
 	require_once('./config.php');
 
-	if(isset($_SESSION['username'])) { 
-		$user=$_SESSION['username'];
-		$user_query="SELECT * FROM users WHERE username='$user'";
-		$user_res=mysqli_query($con, $user_query);
-		$user_row=mysqli_fetch_array($user_res);
-		$user_image_path=substr($user_row['image'], 3);
-	} 
-	else {
-		$signin=ucfirst("Sign In");
+	if (isset($_SESSION['username'])) {
+		$user = $_SESSION['username'];
+		$user_query = "SELECT * FROM users WHERE username='$user'";
+		$user_res = mysqli_query($con, $user_query);
+		$user_row = mysqli_fetch_array($user_res);
+		$user_image_path = substr($user_row['image'], 3);
+	} else {
+		$signin = ucfirst("Sign In");
 	}
 
 
@@ -43,58 +42,59 @@
 	}
 
 	//--------------- path identifying ----------------------------------
-	
-		$path = $sec."/".$page.".php";
-	
+
+	$path = $sec . "/" . $page . ".php";
+
 	?>
 
-	<table style="width: 100%; height: 100%;">
+	<table class="w-full h-full">
 		<tr style="height: 100%;">
 			<td style="width: 15%; height: 100%;" class="fixed p-3 bg-[#16262E] text-white">
 				<div class="flex flex-col w-full h-full justify-between py-3">
-					
+
 					<div>
-						<div class="flex justify-center items-center gap-3 mb-3">
-							<img src="./public/school.png" alt="school logo" class="w-8 h-8 invert">
+						<div class="flex justify-start items-center gap-3 mb-3">
+							
+							<!-- <img src="./public/school.png" alt="school logo" class="w-8 h-8 invert"> -->
 							<h2 class="text-3xl text-white">Menu</h2>
 						</div>
-						<hr>
+						<hr class="mb-2">
 						<ul class="flex flex-col gap-2">
 
 							<a href="?section=grade&page=index">
-								<li class="px-3 py-2 flex flex-row gap-3 <?php echo $sec=='grade' ? 'bg-[#3C7A89]' : '';  ?> hover:bg-[#448A9C] rounded">
+								<li class="px-3 py-2 flex flex-row gap-3 <?php echo $sec == 'grade' ? 'bg-[#3C7A89]' : '';  ?> hover:bg-[#448A9C] rounded">
 									<img src="./public/grade.png" alt="grade img" width="20" height="20" class="invert">
 									Grade
 								</li>
 							</a>
 							<a href="?section=student&page=index">
-								<li class="px-3 py-2 flex flex-row gap-3 <?php echo $sec=='student' ? 'bg-[#3C7A89]' : '';  ?> hover:bg-[#448A9C] rounded">
+								<li class="px-3 py-2 flex flex-row gap-3 <?php echo $sec == 'student' ? 'bg-[#3C7A89]' : '';  ?> hover:bg-[#448A9C] rounded">
 									<img src="./public/student.png" alt="grade img" width="20" height="20" class="invert">
 									Student
 								</li>
 							</a>
 
 							<a href="?section=subject&page=index">
-								<li class="px-3 py-2 flex flex-row gap-3 <?php echo $sec=='subject' ? 'bg-[#3C7A89]' : '';  ?> hover:bg-[#448A9C] rounded">
+								<li class="px-3 py-2 flex flex-row gap-3 <?php echo $sec == 'subject' ? 'bg-[#3C7A89]' : '';  ?> hover:bg-[#448A9C] rounded">
 									<img src="./public/subject.png" alt="grade img" width="20" height="20" class="invert">
 									Subject
 								</li>
 							</a>
-							
+
 						</ul>
 					</div>
 					<div class="flex flex-col w-full gap-4">
 						<h1 class="text-2xl">Profile</h1>
-						<div class="w-full flex flex-row justify-center items-center"  title="Admin Profile Details">
+						<div class="w-full flex flex-row justify-center items-center" title="Admin Profile Details">
 							<img src="./public/bg.jpg" alt="background image" class="w-full h-full  border rounded-lg">
 							<div class="absolute flex flex-col items-center">
 								<img src="<?php echo $user_image_path; ?>" alt="profile image" class="w-20 h-20 object-cover rounded-full border-2">
 								<label class="text-md"><?php echo ucfirst($user); ?></label>
 							</div>
 						</div>
-						
+
 						<a href="auth/logout.php"
-						 class="flex flex-row justify-center items-center gap-3 px-5 py-2 w-full bg-red-500 hover:bg-red-400 text-white rounded">
+							class="flex flex-row justify-center items-center gap-3 px-5 py-2 w-full bg-red-500 hover:bg-red-400 text-white rounded">
 							Logout
 							<img src="./public/out.png" alt="logout img" width="20" height="20" class="invert">
 						</a>
@@ -103,31 +103,31 @@
 			</td>
 			<td style="width: 85%; height: 100%;">
 
-    <div class="fixed top-0 left-[226px] right-0 flex flex-row bg-[#2E4756] justify-between items-center px-4 py-3 z-50">
-        
+				<div class="fixed top-0 w-[85%] flex flex-row bg-[#2E4756] justify-between items-center px-4 py-3 z-50">
 
-        <h1 class="text-4xl text-white">
-            <?php echo ucfirst($sec) . " Details" ?>
-        </h1>
+					<div></div>
+					<h1 class="text-4xl font-semibold text-white">
+						<?php echo ucfirst($sec) . " Details" ?>
+					</h1>
 
-        <a href="?page=create-form&section=<?php echo ucfirst($sec); ?>" 
-           title="<?php echo "Add New " . ucfirst($sec) . " Details"; ?>"
-           class="bg-[#FFBF00] px-6 py-3 text-black w-40 hover:bg-[#CB9800] rounded-[10px]">
-           + Add <?php echo ucfirst($sec); ?>
-        </a>
-    </div>
+					<a href="?page=create-form&section=<?php echo ucfirst($sec); ?>"
+						title="<?php echo "Add New " . ucfirst($sec) . " Details"; ?>"
+						class="bg-[#FFBF00] px-6 py-3 text-center font-semibold text-black w-40 hover:bg-[#CB9800] rounded-[10px]">
+						+ Add <?php echo ucfirst($sec); ?>
+					</a>
+				</div>
 
-    <div class="overflow-auto hide-scrollbar bg-white p-2 pt-20">
-        <?php 
-            if (file_exists($path)) {
-                include_once($path);
-            } else {
-                echo "<h1>404 page not found</h1>";
-            }
-        ?>
-    </div>
+				<div class="overflow-auto hide-scrollbar bg-white p-2 pt-20">
+					<?php
+					if (file_exists($path)) {
+						include_once($path);
+					} else {
+						echo "<div class='flex justify-center items-center'><h1 class='text-3xl text-red-600'>404 page not found</h1></div>";
+					}
+					?>
+				</div>
 
-</td>
+			</td>
 
 		</tr>
 	</table>
